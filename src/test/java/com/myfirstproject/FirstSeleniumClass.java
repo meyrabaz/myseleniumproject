ce7e97d588ecf211fb869c3d6fa3258053ee425d
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-05-02 15:07:04 +0300

    02.05.2023..

--- src/test/java/com/myfirstproject/FirstSeleniumClass.java
+++ src/test/java/com/myfirstproject/FirstSeleniumClass.java
@@ -6,18 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FirstSeleniumClass {
 
     public static void main(String[] args) {
-//        this is one way to create an automation script
+//        This is one way to create an automation script
 //        We will not use main method later on
+
 
 //      For  MAC
         System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");  //LATER WE WILL NOT USE THIS METHOD
+
 
 //       For  WINDOWS
 //        System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");//LATER WE WILL NOT USE THIS METHOD
 
 
+
 //       To  create driver
         WebDriver driver = new ChromeDriver();
+
 
 
 //       To  navigate to techpro
