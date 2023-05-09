d75700c14129b0f577d109c563e8e159e5c64055
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-05-10 01:16:17 +0300

    10.05.2023...

---
+++ src/test/java/com/myfirstproject/utilities/TestBase.java
@@ -0,0 +1,29 @@
+package com.myfirstproject.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+
+    protected static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+}
