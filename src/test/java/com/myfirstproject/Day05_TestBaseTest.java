fac394a5373cc112ee94e0738486657983577201
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-05-09 16:10:59 +0300

    09.05.2023.

---
+++ src/test/java/com/myfirstproject/Day05_TestBaseTest.java
@@ -0,0 +1,12 @@
+package com.myfirstproject;
+
+import com.myfirstproject.utilities.TestBase;
+import org.junit.Test;
+
+public class Day05_TestBaseTest extends TestBase {
+
+    @Test
+    public void testBaseTest(){
+        driver.get("https://techproeducation.com/");
+    }
+}
